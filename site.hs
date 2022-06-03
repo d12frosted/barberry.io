cdbdc6e476ac0434022ac3eac12f63e6a496763d
Boris Buliga <boris@d12frosted.io>
2022-06-03 14:18:13 +0300

    release simple blog with first post

--- site.hs
+++ site.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Monoid (mappend)
 import Hakyll
 
 main :: IO ()
@@ -17,12 +16,58 @@ main = hakyll $ do
     route $ gsubRoute "assets/favicon/" (const "")
     compile copyFileCompiler
 
+  match "intro.org" $ do
+    route $ setExtension "html"
+    compile pandocCompiler
+
+  -- match (fromList ["about.org", "contact.markdown"]) $ do
+  --   route $ setExtension "html"
+  --   compile $
+  --     pandocCompiler
+  --       >>= loadAndApplyTemplate "templates/default.html" defaultContext
+  --       >>= relativizeUrls
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
+
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Archives"
+              `mappend` defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/post-archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
+
   match "index.html" $ do
     route idRoute
     compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      intro <- load "intro.org"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              <> field "intro" (const . return . itemBody $ intro)
+              <> defaultContext
+
       getResourceBody
-        >>= applyAsTemplate defaultContext
-        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
         >>= relativizeUrls
 
   match "templates/*" $ compile templateBodyCompiler
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+  dateField "date" "%B %e, %Y"
+    `mappend` defaultContext
